3b59f432d95b48083caadc1b56a84bbc902d594e
Heyscar Romero <heyscarromero@gmail.com>
2025-07-26 19:25:08 -0300

    Agregar nuevos colores primarios y de éxito en globals.css, ajustar estilos en main-page.css y VideoIntro, y eliminar el archivo de video intro.gif. También se ha añadido un nuevo video intro2.mp4 y se han realizado mejoras en la presentación de los componentes.

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -4,10 +4,10 @@ import { GlassContainer } from "../GlassContainer";
 
 export const Header = () => {
   return (
-    <header className="fixed z-50 top-10 left-0 w-full flex justify-center items-center">
+    <header className="fixed bg-primary-50 z-50 top-10 left-0 w-full flex justify-center items-center">
   
-      <nav className="">
-        <GlassContainer className="py-5 brightness-200">
+      <nav className="nav-glass-container  h-10 flex items-center justify-center !shadow-lg !shadow-white/5 text-white gap-2">
+        
           <Link className="ml-20  my-2" href="/#home">
             Home
           </Link>
@@ -19,7 +19,7 @@ export const Header = () => {
           <Link className="my-2 mr-20" href="/#pricing">
             Pricing
           </Link>
-        </GlassContainer>
+
       </nav>
     </header>
   );
--- components/main-page/VideoIntro.tsx
+++ components/main-page/VideoIntro.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { cn } from "@/lib/utils";
+
 export const VideoIntro = () => {
   return (
     <>
@@ -7,11 +9,18 @@ export const VideoIntro = () => {
       <img
         src="/images/hero-bg.png"
         alt=""
-        className="w-auto  object-cover h-screen mx-auto"
+        className="w-auto  object-cover object-[46.5%_0%] h-screen mx-auto"
       />
 
-      <div className="absolute top-0 left-0 right-0 bottom-0 w-full h-full bg-gradient-to-l from-black/100 via-transparent to-black/100 z-10" />
-      <div className="video-intro absolute top-0 left-0 right-0 bottom-0 ">
+      <div
+        className={cn(
+          "absolute top-0 left-0 right-0 bottom-0 w-full h-full z-10",
+          "sm:bg-gradient-to-l sm:from-black/100 sm:via-transparent sm:to-black/100",
+          "xl:bg-gradient-to-l xl:from-black/100 xl:from-20% xl:via-transparent xl:to-black/100 xl:to-80%",
+       
+        )}
+      />
+      <div className="video-intro-out absolute top-0 sm:left-0 right-0 bottom-0 ">
         <video
           onEnded={() => {
             document.body.classList.add("loaded-intro");
@@ -23,7 +32,7 @@ export const VideoIntro = () => {
           controls={false}
           disablePictureInPicture
           src={`/videos/intro.mp4`}
-          className={` w-auto h-full object-cover mx-auto`}
+          className={` w-auto h-full object-cover object-[46.5%_0%]   mx-auto`}
         />
       </div>
     </>
