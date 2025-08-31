94c2c8ca70b8609378b873f9a1ce3c7250901a50
Heyscar Romero <heyscarromero@gmail.com>
2025-08-31 16:34:34 -0300

    Agregar configuración de Prettier y ESLint, incluyendo archivos de configuración y dependencias necesarias. Se ha creado un archivo .prettierignore para excluir directorios y archivos específicos, y se ha actualizado package.json y pnpm-lock.yaml para incluir nuevas dependencias. También se ha eliminado el archivo eslint.config.mjs obsoleto.

---
+++ eslint.config.js
@@ -0,0 +1,84 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import prettierPlugin from 'eslint-plugin-prettier'
+import prettierConfig from 'eslint-config-prettier'
+import importPlugin from 'eslint-plugin-import'
+import nextPlugin from '@next/eslint-plugin-next'
+export default tseslint.config([
+  {
+    ignores: ['dist', '.next', 'node_modules'],
+  },
+  {
+    files: ['**/*.{ts,tsx,js,jsx}'],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      reactHooks.configs['recommended-latest'],
+      reactRefresh.configs.vite,
+      prettierConfig,
+    ],
+    plugins: {
+      prettier: prettierPlugin,
+      import: importPlugin,
+      '@next/next': nextPlugin,
+    },
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    settings: {
+      'import/resolver': {
+        typescript: {
+          alwaysTryTypes: true,
+          project: './tsconfig.json',
+        },
+        node: {
+          extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        },
+      },
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      'import/no-unresolved': 'error',
+      'import/no-cycle': 'error',
+      'import/no-unused-modules': 'error',
+      'import/no-duplicates': 'error',
+      'import/no-absolute-path': 'error',
+      'import/first': 'error',
+      'import/no-self-import': 'error',
+      // Next.js rules
+      '@next/next/no-html-link-for-pages': 'error',
+      '@next/next/no-img-element': 'warn',
+      '@next/next/no-unwanted-polyfillio': 'error',
+      '@next/next/no-page-custom-font': 'error',
+      '@next/next/no-sync-scripts': 'error',
+      '@next/next/no-typos': 'error',
+      '@next/next/no-css-tags': 'error',
+      '@next/next/no-head-element': 'error',
+      '@next/next/no-document-import-in-page': 'error',
+      '@next/next/no-head-import-in-document': 'error',
+      '@next/next/no-script-component-in-head': 'error',
+      '@next/next/no-before-interactive-script-outside-document': 'error',
+      '@next/next/no-styled-jsx-in-document': 'error',
+      '@next/next/no-duplicate-head': 'error',
+      '@next/next/no-title-in-document-head': 'error',
+      '@next/next/google-font-display': 'warn',
+      '@next/next/google-font-preconnect': 'warn',
+      '@next/next/next-script-for-ga': 'warn',
+      '@next/next/inline-script-id': 'error',
+      // 'import/extensions': [
+      //   'error',
+      //   'ignorePackages',
+      //   {
+      //     js: 'never',
+      //     jsx: 'never',
+      //     ts: 'never',
+      //     tsx: 'never',
+      //   },
+      // ],
+    },
+  },
+])
