ebbf345febb6a8fe9ea7cd2328bede712879d92f
Heyscar Romero <heyscarromero@gmail.com>
2025-08-31 16:38:21 -0300

    Agregar un espacio en blanco en TechnologiesCarrusel.tsx para mejorar la legibilidad del código.

--- components/main-page/TechnologiesCarrusel.tsx
+++ components/main-page/TechnologiesCarrusel.tsx
@@ -14,6 +14,7 @@ import useEmblaCarousel, { EmblaViewportRefType } from "embla-carousel-react"
 import Image from "next/image"
 import { Fragment } from "react"
 
+
 import { cn } from "@/lib/utils"
 import { CarruselContainer } from "../carousel/CarouselContainer"
 import { CarruselItem } from "../carousel/CarouselItem"
